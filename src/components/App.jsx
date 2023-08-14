f37fa96cecd546212c83c66af66094549fb38830
Janith Vemana <janith.ece2007@gmail.com>
2023-08-14 16:41:20 -0400

    handle duplicate entry, move function outside useEffect, clean up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,13 +7,15 @@ import axios from "axios";
 export default function App() {
   const [items, setItems] = useState([]);
   const [toDoItem, setToDoItem] = useState("");
+
   useEffect(() => {
-    async function getItems() {
-      const response = await axios.get(process.env.REACT_APP_SERVER_URL);
-      setItems(response.data.map((item) => item.name));
-    }
     getItems();
   }, []);
+
+  async function getItems() {
+    const response = await axios.get(process.env.REACT_APP_SERVER_URL);
+    setItems(response.data.map((item) => item.name));
+  }
 
   function updateItem(e) {
     const item = e.target.value;
@@ -22,18 +24,23 @@ export default function App() {
 
   async function addItem() {
     const data = { itemName: toDoItem };
-    const response = await axios.post(process.env.REACT_APP_SERVER_URL, data);
-    if (response.status === 200) {
-      setItems((prevItems) => [...prevItems, toDoItem]);
-      setToDoItem("");
+    try {
+      const response = await axios.post(process.env.REACT_APP_SERVER_URL, data);
+      console.log(response.data);
+      if (response.status === 200) {
+        setItems((prevItems) => [...prevItems, toDoItem]);
+        setToDoItem("");
+      }
+    } catch (error) {
+      console.log(error.response);
     }
   }
 
-  async function deleteItem(id) {
-    const config = { data: { itemName: id } };
+  async function deleteItem(itemName) {
+    const config = { data: { itemName: itemName } };
     const response = await axios.delete(process.env.REACT_APP_SERVER_URL, config);
     if (response.status === 200) {
-      setItems(items.filter((i) => i !== id));
+      setItems(items.filter((i) => i !== itemName));
     }
   }
 
